280adc9b4ecefa094e8194203afec36c5de57a04
mike <miguelmtz93@gmail.com>
2013-07-17 05:06:57 +0200

    .

--- src/proyecto/buttonPrompt.java
+++ src/proyecto/buttonPrompt.java
@@ -1,5 +1,162 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mike
+ */
 package proyecto;
 
-public class buttonPrompt {
+import java.awt.Color;
+import java.util.ArrayList;
 
+public class ButtonPrompt extends javax.swing.JDialog{
+	
+	static final long serialVersionUID = 1L;
+	
+	public ButtonPrompt(Form form, Maquina actual){
+    	this.form = form;
+    	this.actual = actual;
+    	initComponents();
+    }
+
+    public String[] toStrings(ArrayList<String> productos) {
+    	String[] strings = new String[productos.size()];
+    	for(int i=0; i<productos.size(); i++)
+    		strings[i] = productos.get(i);
+    	return strings;
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+    	//Linea de codigo de 2 horas
+    	setModalityType(DEFAULT_MODALITY_TYPE);
+    	
+        batch = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        statusLabel = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        productos = new javax.swing.JList();
+        aceptar = new javax.swing.JButton();
+        cancelar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setText("Numero de batch");
+
+        jLabel2.setText("Producto:");
+
+        productos.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = toStrings(actual.getProductos());
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        productos.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jScrollPane1.setViewportView(productos);
+
+        aceptar.setText("Aceptar");
+        aceptar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                aceptarMouseClicked(evt);
+            }
+        });
+
+        cancelar.setText("Cancelar");
+        cancelar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                cancelarMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(batch, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 71, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(cancelar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(aceptar))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(statusLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 269, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(batch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 86, Short.MAX_VALUE)
+                .addComponent(statusLabel)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(aceptar)
+                    .addComponent(cancelar))
+                .addContainerGap())
+        );
+
+        pack();
+    }
+
+    private void aceptarMouseClicked(java.awt.event.MouseEvent evt) {                                     
+    	
+    	try{
+    	form.setBatch(Integer.parseInt(batch.getText()));
+    	}catch(Exception e){
+    		statusLabel.setText("El numero de batch no es valido");
+    		statusLabel.setForeground(Color.RED);
+    		return;
+    	}
+    	
+    	if(productos.getSelectedIndex() == -1)
+    	{
+    		statusLabel.setText("Debes seleccionar un producto");
+    		statusLabel.setForeground(Color.RED);
+    		form.setProducto("");
+    		return;
+    	}
+    	
+    	form.setProducto((String)productos.getSelectedValue());
+    	dispose();
+    }                                    
+
+    private void cancelarMouseClicked(java.awt.event.MouseEvent evt) {                                      
+    	dispose();
+    }                                     
+                   
+    private javax.swing.JButton aceptar;
+    private javax.swing.JButton cancelar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel statusLabel;
+    private javax.swing.JList productos;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField batch;
+    // End of variables declaration      
+    
+    private Form form;
+    private Maquina actual;
 }
